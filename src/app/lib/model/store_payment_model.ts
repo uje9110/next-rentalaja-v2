f19d0f34bb618869c9ebaa7e74311261981e3c00
Ihsan Anwar <ihsananwar398@gmail.com>
2025-09-04 20:22:02 +0700

    little insert in payment creation

--- src/app/lib/model/store_payment_model.ts
+++ src/app/lib/model/store_payment_model.ts
@@ -5,6 +5,7 @@ import {
   StoreOrderPaymentType,
 } from "../types/store_order_payment_type";
 import { Connection } from "mongoose";
+import { StoreOrderType } from "../types/store_order_type";
 
 const PaymentScema = new Schema<StoreOrderPaymentType>(
   {
@@ -20,7 +21,7 @@ const PaymentScema = new Schema<StoreOrderPaymentType>(
       type: String,
       required: true,
     },
-    paidBy: String,
+    paidBy: { type: String },
     orderID: {
       type: String,
     },
@@ -52,6 +53,34 @@ const PaymentScema = new Schema<StoreOrderPaymentType>(
   },
   { timestamps: true },
 );
+
+PaymentScema.statics.createOneStorePayment = async function (
+  subtotal: number,
+  paymentRequest: {
+    paymentAmount: number;
+    paymentType: string;
+    paymentMethod: string;
+  },
+  order: StoreOrderType,
+) {
+  const { paymentType, paymentMethod, paymentAmount } = paymentRequest;
+  let newPaymentAmount;
+  if (paymentType === "full-payment") {
+    newPaymentAmount = subtotal;
+  } else if (paymentType === "partial-payment") {
+    newPaymentAmount = subtotal * 0.2;
+  } else {
+    newPaymentAmount = paymentAmount;
+  }
+
+  // const paymentData: StoreOrderPaymentType = {
+  //   paymentAmount: newPaymentAmount,
+  //   paymentType,
+  //   paymentMethod,
+  //   orderID: order._id,
+  // };
+  const payment = await this.create();
+};
 
 export const createStorePaymentModel = (
   connection: Connection,
